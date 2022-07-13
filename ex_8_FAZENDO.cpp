e24984b6a92577078fe1a2f5e32aef13a258c2a0
Mariana Maia <96018595+maia526@users.noreply.github.com>
2022-07-13 17:00:24 -0300

    Create ex_8_FAZENDO.cpp

---
+++ ex_8_FAZENDO.cpp
@@ -0,0 +1,18 @@
+// Implemente a função
+
+// bool colide_esfera(float x1, float y1, float r1, float x2, float y2, float r2)
+
+// que retorna true se a esfera 1 de centro (x1, y1) e raio r1 está em posição de colisão com a esfera 2
+// de centro (x2, y2) e raio r2, ou false, caso contrário.
+#include <stdio.h>
+bool colide_esfera(float x1, float y1, float r1, float x2, float y2, float r2)
+{
+    
+}
+
+int main()
+{
+    
+}
+
+//PEDIR ESPECIFICAÇÃO SOBRE O QUE É POSIÇÃO DE COLISÃO
